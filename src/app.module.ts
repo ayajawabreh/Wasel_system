0a3a144189df420f5c7a76b61f511cb92b2138f5
NadiaShashtari <s12217187@stu.najah.edu>
2026-03-31 18:07:38 +0300

    auth - login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module'; 
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'advance_soft',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+
+    UserModule,
+    AuthModule, 
+  ],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = { id: 1, email: 'test@test.com', password: await bcrypt.hash('1234', 10), role: 'Admin' };
+    
+    const passwordValid = await bcrypt.compare(password, user.password);
+    if (user && passwordValid) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id, role: user.role };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
